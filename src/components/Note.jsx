c454d526d3349287171dc00a673b7d12ae5ecae3
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-03-05 16:47:44 +0600

    commit kepper

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+    return(
+        <div className="note">
+            <h3>This is Kepper</h3>
+            <p>It's kepper</p>
+        </div>
+    );
+}
+export default Note;
